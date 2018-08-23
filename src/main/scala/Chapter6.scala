d5de2d13e8f13f14d118d77b3ce0df30f0d2853c
nbooo <nbovenko@gmail.com>
2018-08-23 12:18:22 +0300

    Chapter 6: ex. 6.1 - 6.10

---
+++ src/main/scala/Chapter6.scala
@@ -0,0 +1,201 @@
+import scala.annotation.tailrec
+
+object Chapter6 {
+  trait RNG {
+    def nextInt: (Int, RNG)
+  }
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, nextRng) = s(rng)
+      (f(a), nextRng)
+    }
+
+
+  case class SimpleRng(seed: Long) extends RNG {
+
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5deece66dl + 0xBL) & 0xffffffffffffffL
+      val nextRng = SimpleRng(newSeed)
+      val n = (newSeed >> 16).toInt
+      (n, nextRng)
+    }
+  }
+
+  def intRand: Rand[Int] = _.nextInt
+  def doubleRand: Rand[Double] = double
+  def nonNegativeRand: Rand[Int] = rng => nonNegative(rng)
+  /**
+    * Exercise 6.1
+    * */
+  @tailrec
+  def nonNegative(rng: RNG): (Int, RNG) = {
+    def toPositive(n: Int): Option[Int] = n match {
+      case x if x == Int.MinValue => None
+      case x if x < 0             => Some(-x)
+      case x                      => Some(x)
+    }
+
+    val (n, nextRng) = rng.nextInt
+
+    toPositive(n) match {
+      case Some(x) => (x, nextRng)
+      case None    => nonNegative(nextRng)
+    }
+  }
+
+  /**
+    * Exercise 6.2
+    * */
+  @tailrec
+  def double(rng: RNG): (Double, RNG) = {
+    nonNegative(rng) match {
+      case (Int.MaxValue, nextRng)  => double(nextRng)
+      case (value, nextRng)         => (value.toDouble / Int.MaxValue, nextRng)
+    }
+  }
+
+  /**
+    * Exercise 6.3
+    * intDouble, doubleInt, double3
+    * */
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (nextInt, nextRng) = rng.nextInt
+    val (nextDouble, nextRng2) = double(nextRng)
+    ((nextInt, nextDouble), nextRng2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((int, double), nextRng) = intDouble(rng)
+    ((double, int), nextRng)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
+
+  /**
+    * Exercise 6.4
+    * */
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    @tailrec
+    def gen(acc: List[Int], rng: RNG, size: Int): (List[Int], RNG) = {
+      if (size < count) {
+        val (elem, nextRng) = rng.nextInt
+        gen(elem::acc, nextRng, size + 1)
+      }
+      else
+        (acc, rng)
+
+    }
+    gen(List.empty, rng, 0)
+  }
+
+  /**
+    * Exercise 6.5
+    * */
+  def doubleViaMap: Rand[Double] = map(nonNegative)(_ / (Int.MaxValue.toDouble + 1))
+
+  /**
+    * Exercise 6.6
+    * */
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+    val (a, r1) = ra(rng)
+    val (b, r2) = rb(r1)
+    (f(a, b), r2)
+  }
+
+  /**
+    * Exercise 6.7
+    * */
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = rng => {
+    @tailrec
+    def iter(rng: RNG, xs: List[Rand[A]], ys: List[A]): (List[A], RNG) = xs match {
+      case h::t =>
+        val (a, nextRng) = h(rng)
+        iter(nextRng, t, a::ys)
+      case Nil  => (ys.reverse, rng)
+    }
+    iter(rng, fs, List.empty)
+  }
+
+  /**
+    * Exercise 6.8
+    * */
+  def flatMap[A, B](r: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (a, nextRng) = r(rng)
+    g(a)(nextRng)
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    flatMap(nonNegativeRand) {
+      value =>
+        val mod = value % n
+        if (value + (n - 1) - mod >= 0) (rng: RNG) => (mod, rng)
+        else nonNegativeLessThan(n)
+    }
+  }
+
+  /**
+    * Exercise 6.9
+    * map and map2 via flatMap
+    * */
+  def mapViaFlatMap[A, B](r: Rand[A])(op: A => B): Rand[B] = flatMap(r) {
+    a => rng => (op(a), rng)
+  }
+
+  def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(op: (A, B) => C): Rand[C] =
+    flatMap(ra) {
+      a => flatMap(rb) {
+        b => rng => (op(a, b), rng)
+      }
+    }
+
+}
+
+
+/**
+  * Exercise 6.10
+  * Generalised unit, map, map2, flatMap and sequence as members of State class
+  * */
+
+object State {
+
+  def sequence[S, A](states: List[State[S, A]]): State[S, List[A]] = {
+    def iter(s: S, xs: List[State[S, A]], acc: List[A]): List[A] = xs match {
+      case h::t =>
+        val (a, next) = h.run(s)
+        iter(next, t, a::acc)
+      case _    => acc.reverse
+    }
+    State(s => (iter(s, states, List.empty), s))
+
+  }
+  def unit[T, S](a: T): State[S, T] = State(s => (a, s))
+}
+
+case class State[S, +A](run: S => (A, S)) {
+  import State._
+
+  def map[B](f: A => B): State[S, B] = flatMap(a => unit(f(a)))
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap( a =>
+      sb.flatMap(b =>
+        unit(f(a,b))
+      )
+    )
+
+  def flatMap[B](g: A => State[S, B]): State[S, B] = State(s => {
+    val (a, next) = run(s)
+    g(a).run(next)
+  })
+
+}
---
+++ src/test/scala/Chapter6Spec.scala
@@ -0,0 +1,76 @@
+import Chapter6._
+import org.scalatest.{FlatSpec, Matchers}
+import scala.annotation.tailrec
+
+class Chapter6Spec extends FlatSpec with Matchers {
+  private def gen[A](length: Int)(op: Rand[A]): List[A] = {
+    @tailrec
+    def iter(acc: List[A], rng: RNG, count: Int): List[A] = {
+      if (count == 0) acc
+      else {
+        val (i, nextRng) = op(rng)
+        iter(i::acc, nextRng, count - 1)
+      }
+    }
+    iter(List.empty, SimpleRng(302), length)
+  }
+
+  "nonNegative" should "return only positive integers" in {
+    gen(1000000)(nonNegative) forall (_ >= 0) shouldBe true
+  }
+
+  "double" should "return a double in a range [0;1)" in {
+    gen(1000000)(double) forall (n => n >= 0 && n < 1) shouldBe true
+  }
+
+  "ints" should "return a list of integers of a given length" in {
+    val len = 1024
+    val (list: List[Int], _) = ints(len)(SimpleRng(10))
+    list.length shouldBe len
+  }
+
+  "map2" should "compose two values" in {
+    val range = SimpleRng(3)
+
+    val randA = intRand
+    val randB = intRand
+
+    val (a, ra) = randA(range)
+    val (b, _) = randB(ra)
+
+    val (sum, _) = map2(randA, randB)(_ + _)(range)
+
+    sum shouldBe a + b
+  }
+
+  "nonNegativeLessThan" should "generate an integer 0 <= x < n" in {
+    val n = 20
+    val list = gen(10000)(nonNegativeLessThan(n))
+
+    list.forall { value => value >= 0 && value < n } shouldBe true
+  }
+
+  "map2 in State" should "compose two values" in {
+    val rng = SimpleRng(3)
+
+    val sa = State.unit[Int, RNG](3)
+    val sb = State.unit[Int, RNG](6)
+
+    val sum = sa.map2(sb)(_ + _)
+    val (c, _) = sum.run(rng)
+    c shouldBe 9
+  }
+
+  "sequence in State" should "turn a sequence of States into a state of sequence" in {
+    import State.{unit, sequence}
+
+    val u = unit[Int, RNG] _
+    val rng = SimpleRng(34)
+
+    val numbers = List(3, 54, 34, 534, 3123)
+    val states = numbers map u
+
+    val (result, _) = sequence(states).run(rng)
+    result shouldBe numbers
+  }
+}
